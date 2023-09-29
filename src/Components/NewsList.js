25b43030fa9f02f6afe80b253d227aa83b541e92
ishanka158 <96714@fhss.sjp.ac.lk>
2023-09-29 16:25:37 +0530

    react new app

---
+++ src/Components/NewsList.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import NewsItem from "./NewsItem";
+
+const NewsList = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const getArticles = async () => {
+      const response = await axios.get(
+        "https://newsapi.org/v2/top-headlines?country=us&apiKey=YOUR-API-KEY"
+      );
+      console.log(response);
+      setArticles(response.data.articles);
+    };
+    getArticles();
+  }, []);
+
+  return (
+    <div>
+      {articles.map((article) => {
+        return (
+          <NewsItem
+            title={article.title}
+            description={article.description}
+            url={article.url}
+            urlToImage={article.urlToImage}
+          />
+        );
+      })}
+    </div>
+  );
+};
+export default NewsList;
+
+//  157eb9daa307466f9d510b64c4c8b381
